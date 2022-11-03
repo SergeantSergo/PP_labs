a174e462737f40442abc03f16b3badc8ee15b2db
Sergo <matveevsergey1803@gmail.com>
2022-11-03 19:03:35 +0300

    не получилось

--- PPlabs/Entities/DataTransferObjects/PlanManipulationDto.cs
+++ PPlabs/Entities/DataTransferObjects/PlanManipulationDto.cs
@@ -13,7 +13,7 @@ namespace Entities.DataTransferObjects
         [MaxLength(60, ErrorMessage = "Maximum length for the Name is 60 characters.")]
         public string Name { get; set; }
         [Required(ErrorMessage = "Kolvo is a required field.")]
-        [Range(1, int.MinValue, ErrorMessage = "The quantity cannot be less than 1")]
+        [Range(0, int.MaxValue, ErrorMessage = "The quantity cannot be less than 1")]
         public int Kolvo { get; set; }
         [Required(ErrorMessage = "Date is a required field.")]
         public long Date { get; set; }
